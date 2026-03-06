fe9ee4ab451181349b115a11f7e1e2cd5fa065e3
izumi-09 <mahn54592@gmail.com>
2026-03-06 17:29:49 +0500

    Create Hello-world.groovy
    jenkins shared libarary

---
+++ vars/Hello-world.groovy
@@ -0,0 +1,10 @@
+class Example { 
+   static void main(String[] args) { 
+      //Initializing 2 variables 
+      int x = 5; 
+      int X = 6; 
+	  
+      //Printing the value of the variables to the console 
+      println("The value of x is " + x + "The value of X is " + X);  
+   }
+}
