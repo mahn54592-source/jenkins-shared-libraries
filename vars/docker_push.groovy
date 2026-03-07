1bd7201e83365ebb46d3e86019d57fbe4fe00d57
izumi-09 <mahn54592@gmail.com>
2026-03-08 02:20:00 +0500

    Update docker_push.groovy
    docker to dockerHubCred

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,7 +1,7 @@
 def call(String Project, String ImageTag, String dockerUser){
-  withCredentials([usernamePassword(credentialsId: 'docker', 
-                                   passwordVariable: 'dockerhubpass', 
-                                   usernameVariable: 'dockerhubuser')]) {
+  withCredentials([usernamePassword(credentialsId: "dockerhubCred", 
+                                   passwordVariable: 'dockerHubPass', 
+                                   usernameVariable: 'dockerHubUser')]) {
       sh "docker login -u ${dockerUser} -p ${dockerhubpass}"
   }
   sh "docker push ${dockerUser}/${Project}:${ImageTag}"
